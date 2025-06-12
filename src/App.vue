995ddff244e89a4325f2b8755f2ef13b2812dff4
AvaniSujith <chinchuavani123@gmail.com>
2025-06-12 17:13:08 +0530

    style:base styling for the container

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,44 @@
+<script setup>
+// import imgUrl from './'
+</script>
+
 <template>
   <div class="outer-container">
-    
+    <header>
+      <img src="/public/notepad.png">
+      <h2>ToDo List</h2>
+    </header>
   </div>
 </template>
+
+<style>
+.outer-container{
+  background-color: #fff;
+  height: 650px;
+  max-width: 60%;
+  width: 100%;
+  margin: auto;
+  border-radius: 12px;
+  box-shadow: rgba(0, 0, 0, 0.15) 0px 5px 15px 0px; 
+  padding: 35px;
+}
+
+header{
+  display: flex;
+  align-items: center;
+  gap: 6px;
+}
+
+img{
+  height: 45px;
+  width: auto;
+}
+
+h2{
+  font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+  font-size: 40px;
+  line-height: 45px;
+  margin: 0;
+  padding-top: 5px;
+}
+</style>
