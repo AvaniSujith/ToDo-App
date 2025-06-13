75a4410729e91ccc3ffdc4384384f6e30c335d3f
AvaniSujith <chinchuavani123@gmail.com>
2025-06-13 13:29:19 +0530

    updation: Correction in handleInput function

--- src/components/InputBar.vue
+++ src/components/InputBar.vue
@@ -1,29 +1,30 @@
 <script setup>
+const props = defineProps(["value", "placeholder"]);
 
-    const props = defineProps([
-        'value',
-        'placeholder'
-    ]);
-    
-    const emit = defineEmits(['input']);
+const emit = defineEmits(["input"]);
 
-    const handleInput = (event) => {
-        emit('update:value', event.target.value)
-    }
+const handleInput = (event) => {
+  const inputValue = event.target.value;
 
+  emit("input", inputValue);
+};
 </script>
 
 <template>
-    <input type="text" :placeholder="placeholder" :value="value" @input="handleInput" />
+  <input
+    class="input-bar"
+    type="text"
+    :placeholder="placeholder"
+    :value="value"
+    @input="handleInput"
+  />
 </template>
 
 <style>
-
-input{
-    border: 2px solid #eee;
-    padding: 8px;
-    border-radius: 8px;
-    margin: 10px 0px;
+.input-bar {
+  border: 2px solid #eee;
+  padding: 8px;
+  border-radius: 8px;
+  margin: 10px 0px;
 }
-
 </style>
