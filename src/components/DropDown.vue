46654a52cbf27eb4175e43966309fc2da6ea2c81
AvaniSujith <chinchuavani123@gmail.com>
2025-06-16 14:03:29 +0530

    feat: add dropdown for filteration

---
+++ src/components/DropDown.vue
@@ -0,0 +1,16 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="dropdown-container">
+        <select name="category" id="task-category">
+            <option value="all">All</option>
+            <option value="complete">Completed</option>
+            <option value="incomplete">Incomplete</option>
+        </select>
+    </div>
+</template>
+
+<style>
+</style>
