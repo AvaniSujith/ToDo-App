dea050e72ef6e7a393d557b7b9bb36d53b72f2b5
AvaniSujith <chinchuavani123@gmail.com>
2025-06-17 14:50:49 +0530

    refactor: missing store

--- src/components/AddNewTask.vue
+++ src/components/AddNewTask.vue
@@ -1,23 +1,33 @@
 <script setup>
-import InputBar from './InputBar.vue';
+import { ref } from "vue";
+
+import InputBar from "./InputBar.vue";
+
+const taskTitle = ref("");
+
+const handleNewTask = (value) => (taskTitle.value = value);
 </script>
 
 <template>
-    <div class="add-task-container">
-          <input-bar :model-value="taskAdded" placeholder="New todo"/>
-        <button class="add-task-btn">Add</button>
-    </div>
+  <div class="add-task-container">
+    <input-bar
+      :model-value="taskTitle"
+      placeholder="New todo"
+    />
+    <button class="add-task-btn"  @click="handleNewTask">Add</button>
+    <p>{{ taskTitle }}</p>
+  </div>
 </template>
 
 <style>
-.add-task-container{
-    display: flex;
-    align-items: center;
-    gap: 3px;
-    width: 100%;
+.add-task-container {
+  display: flex;
+  align-items: center;
+  gap: 3px;
+  width: 100%;
 }
-.add-task-btn{
- background-color: #005CC8;
+.add-task-btn {
+  background-color: #005cc8;
   padding: 8px;
   border-radius: 8px;
   color: #fff;
